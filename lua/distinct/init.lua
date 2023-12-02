b312c96f52d2c9caa5c387e4714efad9de7eb133
Bartek Jaszczak <bartlomiej.jaszczak@proton.me>
2023-12-02 23:48:25 +0100

    Add settings

--- lua/distinct/init.lua
+++ lua/distinct/init.lua
@@ -1,10 +1,23 @@
 local M = {}
+local setup_done = false
+
+local default_settings = {
+    doc_comments_different_color = true,
+}
 
 local function set_highlights(theme)
     local highlights = require("distinct.highlights").get_highlights(theme)
     for hl, opts in pairs(highlights) do
         vim.api.nvim_set_hl(0, hl, opts)
     end
+end
+
+M.settings = {}
+
+function M.setup(settings)
+    M.settings = vim.tbl_deep_extend("force", default_settings, settings or {})
+
+    setup_done = true
 end
 
 function M.load()
@@ -20,15 +33,14 @@ function M.load()
     vim.o.termguicolors = true
     vim.g.colors_name = "distinct"
 
-    -- Temporary
-    local settings = {
-        doc_comments_different_color = true,
-    }
+    if not setup_done then
+        M.setup()
+    end
 
     -- Enable highlights
     local colors = require("distinct.colors")
     local theme = require("distinct.theme")
-    theme = theme.get_theme(colors, settings)
+    theme = theme.get_theme(colors, M.settings)
 
     set_highlights(theme)
 end
